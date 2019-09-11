304a6f6e4eee1c9d1b4cfd47bb82f7d6778608da
Akshay Mittur <53973656+akshaymitturlab@users.noreply.github.com>
2019-09-11 15:05:26 +0530

    Create ascii.asm

---
+++ ascii.asm
@@ -0,0 +1,49 @@
+.MODEL SMALL
+
+.DATA
+        MSG1 DB 0DH, 0AH, "ENTER ALPHANUMERIC$"
+    RES DB 02 DUP(0)
+
+.CODE
+    MOV AX, @DATA
+    MOV DS, AX
+    LEA DX, MSG1
+    CALL DISP
+    MOV AH, 01H
+    INT 21H
+    MOV BL, AL
+    MOV CL, 4
+    SHR AL, CL
+        CMP AL, 0AH
+    JC DIGIT
+        ADD AL, 07
+
+DIGIT:    ADD AL, 30H
+    MOV RES, AL
+    AND BL, 0FH
+    CMP BL, 00H
+    JC DIGIT1
+       
+
+DIGIT1:    ADD BL, 30H
+    MOV RES+1, BL
+    MOV AH, 00H
+    MOV AL, 03H
+    INT 10H
+        MOV AH, 02H
+    MOV BH, 00H
+    MOV DH, 0CH
+        MOV DL, 28H
+    INT 10H
+    MOV RES+2, '$'
+    LEA DX, RES
+    CALL DISP
+    MOV AH, 4CH
+    INT 21H
+DISP PROC NEAR
+        MOV AH, 09H
+    INT 21H
+    RET 
+    DISP ENDP
+    END
+
